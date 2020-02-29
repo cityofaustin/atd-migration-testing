fbb1a8341bc3b3e3fe1a09a9590725ee9dd9f2a4
johnclary <john.clary@austintexas.gov>
2020-02-29 13:07:44 -0600

    happy label creator

--- 4_create_labels.py
+++ 4_create_labels.py
@@ -1,23 +1,68 @@
+"""
+Make sure all labels are created in the destination repo.
 
+We do this by:
+1. Downloading all labels in the destination repo
+2. Downloading all labels in the source repo (which gives us name/color/description)
+3. Loading new product labels from the label tracking worksheet
+4. Extracting all labels from the source issues, which have already been mapped to new product labels
+5. Identifying labels from the source issues are missing from the destination
+6. Creating missing labels in the destination repo
+
+"""
 import csv
 import json
+from os import listdir
+from os.path import isfile, join
 import pdb
 
 import requests
 
-from config.config import DEST_REPO, LABEL_FILE, SOURCE_REPOS
+from config.config import DEST_REPO, DIR_LABELED, LABEL_FILE, SOURCE_REPOS
 
 from config.secrets import GITHUB_USER, GITHUB_PASSWORD
 from _logger import get_logger
 
-"""
-make sure all labels are created in the destination repo
 
-TODO: map labels before creating missing
-TODO: change dest repo
-"""
+def get_missing_labels(dest_labels, label_defs, issue_labels):
+    return [label_defs[label] for label in label_defs.keys() if label not in dest_labels and label in issue_labels]
 
-def get_labels(repo_name):
+
+def get_labels_from_csv(fname):
+    with open(fname, "r") as fin:
+        reader = csv.DictReader(fin)
+        return {label["name"] : { "name" : label["name"], "color" : label["color"], "description" : label["description"]} for label in reader if label["action"] != "map"}
+
+
+def get_labels_from_issues(source_directory):
+        # get all labels from all issues
+    fnames = [join(source_directory, f) for f in listdir(source_directory) if isfile(join(source_directory, f)) and f.endswith(".json")]
+    
+    labels = []
+
+    for fname in fnames:
+        with open(fname, "r") as fin:
+            issue = json.loads(fin.read())
+            if issue.get("labels"):
+                labels.extend(issue["labels"])
+
+    return list(set(labels))
+
+
+def get_labels_from_source_repos(repos):
+    source_labels = {}
+
+    for repo in repos:
+        labels = get_labels_from_repo(repo["name"])
+        
+        for label in labels:
+            if label["name"] not in source_labels:
+                source_labels[label["name"]] = label
+
+    return source_labels
+
+
+def get_labels_from_repo(repo_name):
     labels = []
 
     url = f"https://api.github.com/repos/cityofaustin/{repo_name}/labels"
@@ -79,48 +124,34 @@ def create_label(label, repo_name):
     res = requests.post(url, json=payload, auth=(GITHUB_USER, GITHUB_PASSWORD))
 
     res.raise_for_status()
+
+    logger.info(f"{payload['name']}")
     
     return None
 
 
 def main():
+    # get all labels from destination repo (as list)
+    dest_labels = get_labels_from_repo(DEST_REPO)
 
-    # read in new labels we've defined in a csv, excluding those that will be mapped to another label
-    with open(LABEL_FILE, "r") as fin:
-        reader = csv.DictReader(fin)
-        loaded_labels = [row for row in reader if row["action"] != "map"]
+    dest_labels = [label["name"] for label in dest_labels]
 
-    labels = [{"name": label["name"], "color":label["color"], "description" : label["description"] } for label in loaded_labels]
+    # get all labels from source repos
+    source_labels = get_labels_from_source_repos(SOURCE_REPOS)
 
-    # get all labels from all repos
-    for repo in SOURCE_REPOS:
-        repo_labels = get_labels(repo["name"])
-        labels.extend(repo_labels)
+    # get all labels from label file
+    file_labels = get_labels_from_csv(LABEL_FILE)
 
-    labels = reduce_labels(labels)
+    # merge label definitions from file and source repos
+    source_labels.update(file_labels)
 
-    # get all labels from destination repo:
-    dest_labels = get_labels(DEST_REPO)
+    # get all labels from labeled issues
+    issue_labels = get_labels_from_issues(DIR_LABELED)
 
-    create_labels = []
+    create_labels = get_missing_labels(dest_labels, source_labels, issue_labels)
 
-    for label in labels:
-        matched = False
-
-        name = label["name"]
-        
-        for dest_label in dest_labels:
-            if dest_label["name"] == name:
-                matched = True
-                break
-
-        if not matched:
-            create_labels.append(label)
-
-    # TODO: map labels
     for label in create_labels:
         logger.info(f"{label['name']}")
-        pdb.set_trace() 
         create_label(label, DEST_REPO)
 
 
