5ad50d665ab2a9bdddc0e56710b4315a06e42e8e
johnclary <john.clary@austintexas.gov>
2020-02-29 17:13:03 -0600

    create comments

---
+++ 8_create_comments.py
@@ -0,0 +1,75 @@
+"""
+Update new github issues w/ comments
+"""
+import json
+from os import listdir, remove
+from os.path import isfile, join
+import pdb
+
+import requests
+
+from config.config import DIR, DEST_REPO
+from config.secrets import GITHUB_USER, GITHUB_PASSWORD
+
+from _logger import get_logger
+
+
+def write_issue(issue, dest):
+    fname = issue["path"]
+
+    with open(fname, "w") as fout:
+        fout.write(json.dumps(issue))
+
+    return True
+
+
+def create_comment(issue_number, repo, body):
+
+    url = f"https://api.github.com/repos/cityofaustin/{repo}/issues/{issue_number}/comments"
+
+    res = requests.post(url, json={"body": body}, auth=(GITHUB_USER, GITHUB_PASSWORD))
+
+    res.raise_for_status()
+
+    logger.info(f"{issue_number} {repo} {body}")
+    return res.json()
+
+
+def load_issues(source_dir):
+    issues = []
+
+    fnames = [
+        join(source_dir, f)
+        for f in listdir(source_dir)
+        if isfile(join(source_dir, f)) and f.endswith(".json")
+    ]
+
+    for fname in fnames:
+        with open(fname, "r") as fin:
+            issue = json.loads(fin.read())
+            issues.append(issue)
+
+    return issues
+
+
+def main():
+    issues = load_issues(DIR)
+
+    for issue in issues:
+        # skip issues which have not been created:
+        issue_number = issue.get("migration").get("new_issue_number")
+
+        if not issue_number:
+            continue
+
+        # skip issues which already have comments created
+        if issue.get("comments"):
+            for comment in issue["comments"]:
+                if not comment.get("uploaded"):
+                    res = create_comment(issue_number, DEST_REPO, comment["body"])
+                    comment["uploaded"] = True
+                    write_issue(issue, DEST_REPO)
+
+if __name__ == "__main__":
+    logger = get_logger("github_create_comments")
+    main()
