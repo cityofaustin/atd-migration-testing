b4322a3433c028117eb3195ba480a48fdc700c51
johnclary <john.clary@austintexas.gov>
2020-02-29 12:19:55 -0600

    repo map

--- config/config.py
+++ config/config.py
@@ -1,5 +1,6 @@
 DIR_GITHUB = "data/github"
 DIR_ZENHUB = "data/zenhub"
+DIR_LABELED = "data/labeled"
 FAILDIR = "data/zenhub_errors"
 LABEL_FILE = "migration_label_tracking.csv"
 # DEST_REPO = "atd-data-tech"
@@ -32,3 +33,25 @@ SOURCE_REPOS = [
     # {"id": 183474824, "name": "atd-service-bot"},
     # {"id": 178280308, "name": "atd-vz-data"},
 ]
+
+
+REPO_MAP = {
+    "atd-amanda" : "Product: AMANDA",
+    "atd-geospatial" : "Service: Geo",
+    "atd-knack-data-tracker" : "Product: AMD Data Tracker",
+    "atd-knack-development-review" : "Product: Transportation Development Services",
+    "atd-knack-dts-portal" : "Product: AMD Data Tracker",
+    "atd-knack-finance-purchasing" : "Product: Finance & Inventory",
+    "atd-knack-forms" : "Product: ATD Forms",
+    "atd-knack-signs-markings" : "Product: Signs & Markings",
+    "atd-knack-street-banner" : "Product: Banners",
+    "atd-knack-visitor-log" : "Product: Visitor Log",
+    "atd-mobile-signals-work-orders" : "Product: Mobile Signal Work Orders",
+    "atd-mobility-project-database" : "Product: Mobility Project Database",
+    "atd-residential-parking-permits" : "Product: Residential Parking Permit Digitization",
+    "atd-vz-data" : "Product: Vision Zero Crash Data System",
+    "atd-dockless-dataviz" : "Product: Dockless Dataviz",
+    "atd-finance-scraper" : "Product: Finance & Inventory",
+    "atd-data-and-performance" : "Product: Data & Performance Hub",
+    "atd-dockless-processing" : "Product: Dockless Dataviz",
+}
