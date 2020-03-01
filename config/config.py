6bb9fc5683a453169e7cb4d0ed62328270c6f920
johnclary <john.clary@austintexas.gov>
2020-03-01 12:48:11 -0600

    new repo config

--- config/config.py
+++ config/config.py
@@ -1,34 +1,35 @@
 DIR = "data"
 LABEL_FILE = "migration_label_tracking.csv"
-# DEST_REPO = "atd-data-tech"
 DEST_REPO = "atd-migration-testing"
+DEST_REPO_ID = 235453293 # atd-migration-testing
 WORKSPACE_ID = "5caf7dc6ecad11531cc418ef" # DTS ZENHUB Workspace
 
 SOURCE_REPOS = [
-    # {"id": 183466621, "name": "atd-amanda"},
-    # {"id": 117182832, "name": "atd-cctv-service"},
-    # {"id": 136938110, "name": "atd-data-deploy"},
-    {"id": 67241163, "name": "atd-data-publishing"},
-    # {"id": 167410745, "name": "atd-data-service"},
-    # {"id": 191611348, "name": "atd-data-utilities"},
-    # {"id": 153001935, "name": "atd-dockless-api"},
-    # {"id": 153001229, "name": "atd-dockless-dataviz"},
-    # {"id": 160746380, "name": "atd-dockless-processing"},
-    # {"id": 140850398, "name": "atd-finance-scraper"},
-    {"id": 182321690, "name": "atd-geospatial"},
-    # {"id": 181022917, "name": "atd-knack-data-tracker"},
-    # {"id": 204069168, "name": "atd-knack-development-review"},
-    # {"id": 187703251, "name": "atd-knack-dts-portal"},
-    # {"id": 182864883, "name": "atd-knack-finance-purchasing"},
-    # {"id": 187698234, "name": "atd-knack-forms"},
-    # {"id": 191638408, "name": "atd-knack-signs-markings"},
-    # {"id": 182868172, "name": "atd-knack-street-banner"},
-    # {"id": 187706175, "name": "atd-knack-visitor-log"},
-    # {"id": 149492871, "name": "atd-mobile-signals-work-orders"},
-    # {"id": 185268238, "name": "atd-mobility-project-database"},
-    # {"id": 138333607, "name": "atd-residential-parking-permits"},
-    # {"id": 183474824, "name": "atd-service-bot"},
-    # {"id": 178280308, "name": "atd-vz-data"},
+    # {"id" : 55646931, "name" : "atd-data-and-performance"},
+    # {"id" : 67241163, "name" : "atd-data-publishing"},
+    # {"id" : 136938110, "name" : "atd-data-deploy"},
+    # {"id" : 138333607, "name" : "atd-residential-parking-permits"},
+    # {"id" : 140626918, "name" : "atd-data-tech"},
+    # {"id" : 140850398, "name" : "atd-finance-scraper"},
+    # {"id" : 149492871, "name" : "atd-mobile-signals-work-orders"},
+    # {"id" : 153001229, "name" : "atd-dockless-dataviz"},
+    # {"id" : 153001935, "name" : "atd-dockless-api"},
+    # {"id" : 160746380, "name" : "atd-dockless-processing"},
+    # {"id" : 164503017, "name" : "lunch-near-atd-cameron"},
+    # {"id" : 178280308, "name" : "atd-vz-data"},
+    # {"id" : 181022917, "name" : "atd-knack-data-tracker"},
+    {"id" : 182321690, "name" : "atd-geospatial"},
+    # {"id" : 182864883, "name" : "atd-knack-finance-purchasing"},
+    # {"id" : 182868172, "name" : "atd-knack-street-banner"},
+    # {"id" : 183466621, "name" : "atd-amanda"},
+    # {"id" : 185268238, "name" : "atd-mobility-project-database"},
+    # {"id" : 187703251, "name" : "atd-knack-dts-portal"},
+    # {"id" : 191638408, "name" : "atd-knack-signs-markings"},
+    # {"id" : 197414350, "name" : "atd-gridsmart"},
+    # {"id" : 209419491, "name" : "atd-micromobility-aggregation-privacy"},
+    # {"id" : 213555617, "name" : "atd-airflow"},
+    # {"id" : 232883213, "name" : "atd-mds"},
+    # {"id" : 235453293, "name" : "atd-migration-testing"},
 ]
 
 
@@ -51,4 +52,5 @@ REPO_MAP = {
     "atd-finance-scraper" : "Product: Finance & Inventory",
     "atd-data-and-performance" : "Product: Data & Performance Hub",
     "atd-dockless-processing" : "Product: Dockless Dataviz",
+    "atd-micromobility-aggregation-privacy" : "Product: Dockless Dataviz",
 }
