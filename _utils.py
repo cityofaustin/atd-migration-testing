0848b8d031828e54c949e3c9ee06589f97198cca
johnclary <john.clary@austintexas.gov>
2020-03-01 12:48:29 -0600

    new utils lib

---
+++ _utils.py
@@ -0,0 +1,20 @@
+import json
+from os import listdir, remove
+from os.path import isfile, join
+
+def load_issues(source_dir):
+    issues = []
+
+    fnames = [
+        join(source_dir, f)
+        for f in listdir(source_dir)
+        if isfile(join(source_dir, f)) and f.endswith(".json")
+    ]
+
+    for fname in fnames:
+        with open(fname, "r") as fin:
+            print(fname)
+            issue = json.loads(fin.read())
+            issues.append(issue)
+
+    return issues
